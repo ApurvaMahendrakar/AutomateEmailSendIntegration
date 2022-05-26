2f7e67a39e0b9cdf85fb3d140222c9019735a121
apurva <apurva@DESKTOP-K1R1GG0>
2022-05-26 13:23:53 +0530

    1.0

---
+++ AutomateEmailSendProject/src/main/java/POMConfig/ConfigurationPOM.java
@@ -0,0 +1,10 @@
+package POMConfig;
+
+public class ConfigurationPOM {
+	
+
+	public String attachFile = "D:\\AutomateEmailSendProject\\src\\test\\resources\\demo.txt";
+	public String propertiesFile = "D:\\AutomateEmailSendProject\\src\\main\\resources\\config.properties";
+
+
+}
